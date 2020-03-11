79fa22825b7ea7981cbbb65b8296ca60659dd24a
leehi0210 <leehi0210@gmail.com>
2020-03-11 22:13:36 +0900

    add utils

--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -6,7 +6,34 @@
 /*   By: daelee <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/11 18:03:34 by daelee            #+#    #+#             */
-/*   Updated: 2020/03/11 18:03:35 by daelee           ###   ########.fr       */
+/*   Updated: 2020/03/11 22:12:55 by daelee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef FT_ARRAY_H
+# define FT_ARRAY_H
+
+#include <stdlib.h>
+
+# define DEFAULT_ARRAY_SIZE 5000
+
+typedef struct		s_array
+{
+	int		        size;
+	char	        *str;
+}					t_array;
+
+t_array			*ft_create_array(void);
+t_array			*ft_append_str(t_array *arr, char *str);
+size_t          ft_strlen(const char *s);
+size_t          ft_strlcpy(char *dest, const char *src, size_t dstsize);
+size_t          ft_strlcat(char *dest, const char *src, size_t dstsize);
+char			**ft_malloc_error(char **tab);
+unsigned int    ft_get_nb_strs(char const *s, char c);
+void			ft_get_next_str(char **next_str, unsigned int *next_str_len, char c);
+char		    **ft_split(char const *s, char c);
+int             get_next_line(int fd, char **line);
+
+
+
+#endif
--- get_next_line/get_next_line_utils.c
+++ get_next_line/get_next_line_utils.c
@@ -6,7 +6,183 @@
 /*   By: daelee <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/11 18:03:53 by daelee            #+#    #+#             */
-/*   Updated: 2020/03/11 18:04:00 by daelee           ###   ########.fr       */
+/*   Updated: 2020/03/11 22:08:32 by daelee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
+
+t_array		*ft_create_array(void)
+{
+	t_array *array;
+
+	array = (t_array*)malloc(sizeof(t_array));
+	array->size = DEFAULT_ARRAY_SIZE;
+	array->str = (char *)malloc(sizeof(char) * array->size);
+	array->str[0] = 0;
+	return (array);
+}
+
+t_array		*ft_resize_array(t_array *arr)
+{
+	char	*new_str;
+	int		new_size;
+
+	new_size = arr->size * 2;
+	new_str = (char *)malloc(sizeof(char) * new_size);
+	ft_strcpy(new_str, arr->str);
+	free(arr->str);
+	arr->str = new_str;
+	arr->size = new_size;
+	return (arr);
+}
+
+t_array		*ft_append_str(t_array *arr, char *str)
+{
+	while (ft_strlen(arr->str) + ft_strlen(str) + 2 >= arr->size)
+		ft_resize_array(arr);
+	ft_strcat(arr->str, str);
+	return (arr);
+}
+
+size_t      ft_strlen(const char *s)
+{
+    size_t  i;
+
+    i = 0;
+    while (s[i])
+        i++;
+    return (i);
+}
+
+size_t      ft_strlcpy(char *dest, const char *src, size_t dstsize)
+{
+    size_t	src_len;
+	size_t	i;
+
+	src_len = 0;
+	while (src[src_len] != '\0')
+	{
+		src_len++;
+	}
+	if (dstsize == 0)
+	{
+		return (src_len);
+	}
+	i = 0;
+	while (src[i] != '\0' && i < (dstsize - 1))
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (src_len);
+}
+
+size_t      ft_strlcat(char *dest, const char *src, size_t dstsize)
+{
+    size_t	i;
+	size_t	dest_len;
+	size_t	src_len;
+
+	dest_len = ft_strlen(dest);
+	src_len = ft_strlen(src);
+	i = 0;
+	while (src[i] != '\0' && dest_len + 1 + i < dstsize)
+	{
+		dest[dest_len + i] = src[i];
+		i++;
+	}
+	dest[dest_len + i] = '\0';
+	if (dstsize < dest_len)
+		return (dstsize + src_len);
+	return (dest_len + src_len);
+}
+
+static char			**ft_malloc_error(char **tab)
+{
+	unsigned int	i;
+
+	i = 0;
+	while (tab[i])
+	{
+		free(tab[i]);
+		i++;
+	}
+	free(tab);
+	return (NULL);
+}
+
+static unsigned int	ft_get_nb_strs(char const *s, char c)
+{
+	unsigned int	i;
+	unsigned int	nb_strs;
+
+	if (!s[0])
+		return (0);
+	i = 0;
+	nb_strs = 0;
+	while (s[i] && s[i] == c) //c가 제일 앞에, 여러 개 연속으로 있을 때 패스
+		i++;
+	while (s[i])
+	{
+		if (s[i] == c)
+		{
+			nb_strs++;
+			while (s[i] && s[i] == c)
+				i++;
+			continue ;
+		}
+		i++;
+	}
+	if (s[i - 1] != c)
+		nb_strs++; //마지막이 c로 안 끝났으면 +1
+	return (nb_strs);
+}
+
+static void			ft_get_next_str(char **next_str, unsigned int *next_str_len,
+					char c)
+{
+	unsigned int i;
+
+	*next_str += *next_str_len; //실제 next_str 주소 ++
+	*next_str_len = 0;
+	i = 0;
+	while (**next_str && **next_str == c)
+		(*next_str)++;
+	while ((*next_str)[i]) //갯수 샐 때는 지역변수 i 이용
+	{
+		if ((*next_str)[i] == c)
+			return ;
+		(*next_str_len)++;
+		i++;
+	}
+}
+
+char				**ft_split(char const *s, char c)
+{
+	char			**tab;
+	char			*next_str;
+	unsigned int	next_str_len;
+	unsigned int	nb_strs;
+	unsigned int	i;
+
+	if (!s)
+		return (NULL);
+	nb_strs = ft_get_nb_strs(s, c);
+	if (!(tab = (char **)malloc(sizeof(char *) * (nb_strs + 1)))) //마지막 tab[i]에도 NULL보장
+		return (NULL);
+	i = 0;
+	next_str = (char *)s;
+	next_str_len = 0;
+	while (i < nb_strs)
+	{
+		ft_get_next_str(&next_str, &next_str_len, c);
+		if (!(tab[i] = (char *)malloc(sizeof(char) * (next_str_len + 1))))
+			return (ft_malloc_error(tab));
+		ft_strlcpy(tab[i], next_str, next_str_len + 1);
+		i++;
+	}
+	tab[i] = NULL;
+	return (tab);
+}
