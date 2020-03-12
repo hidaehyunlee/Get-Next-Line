934406b4d7056bedae6e4b7e3598e1f68a344210
leehi0210 <leehi0210@gmail.com>
2020-03-13 00:00:32 +0900

    test gong

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,11 +6,28 @@
 /*   By: daelee <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/11 17:57:29 by daelee            #+#    #+#             */
-/*   Updated: 2020/03/11 18:41:57 by daelee           ###   ########.fr       */
+/*   Updated: 2020/03/12 23:59:49 by daelee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int     get_next_line(int fd, char **line)
+#include "get_next_line.h"
+
+int                 get_next_line(int fd, char **line)
 {
-    
+    char            *buf;
+    static t_array  *temp;
+    int             eof;
+
+    if (!(buf = (char *)malloc(sizeof(char)*BUFFER_SIZE + 1)))
+        return (NULL);
+    temp = ft_create_array();
+    while (ft_strchr(temp, '\n') == 0)
+    {
+        eof = read(fd, buf, BUFFER_SIZE) //eof 따로 뺀 이유 -> 0이어도 read_line을 수행해야해서.
+        ft_append_str(temp, buf);
+        read_line(temp, *line); 
+        if (eof == 0)
+            return (0) //eof
+    }
+    return (1);
 }
--- get_next_line/get_next_line_utils.c
+++ get_next_line/get_next_line_utils.c
@@ -6,7 +6,7 @@
 /*   By: daelee <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/11 18:03:53 by daelee            #+#    #+#             */
-/*   Updated: 2020/03/11 22:08:32 by daelee           ###   ########.fr       */
+/*   Updated: 2020/03/12 22:49:03 by daelee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -99,90 +99,20 @@ size_t      ft_strlcat(char *dest, const char *src, size_t dstsize)
 	return (dest_len + src_len);
 }
 
-static char			**ft_malloc_error(char **tab)
+char        *ft_strchr(const char *s, int c)
 {
-	unsigned int	i;
+    char    find;
+    int     i;
 
-	i = 0;
-	while (tab[i])
-	{
-		free(tab[i]);
-		i++;
-	}
-	free(tab);
-	return (NULL);
-}
-
-static unsigned int	ft_get_nb_strs(char const *s, char c)
-{
-	unsigned int	i;
-	unsigned int	nb_strs;
-
-	if (!s[0])
-		return (0);
-	i = 0;
-	nb_strs = 0;
-	while (s[i] && s[i] == c) //c가 제일 앞에, 여러 개 연속으로 있을 때 패스
-		i++;
-	while (s[i])
-	{
-		if (s[i] == c)
-		{
-			nb_strs++;
-			while (s[i] && s[i] == c)
-				i++;
-			continue ;
-		}
-		i++;
-	}
-	if (s[i - 1] != c)
-		nb_strs++; //마지막이 c로 안 끝났으면 +1
-	return (nb_strs);
-}
-
-static void			ft_get_next_str(char **next_str, unsigned int *next_str_len,
-					char c)
-{
-	unsigned int i;
-
-	*next_str += *next_str_len; //실제 next_str 주소 ++
-	*next_str_len = 0;
-	i = 0;
-	while (**next_str && **next_str == c)
-		(*next_str)++;
-	while ((*next_str)[i]) //갯수 샐 때는 지역변수 i 이용
-	{
-		if ((*next_str)[i] == c)
-			return ;
-		(*next_str_len)++;
-		i++;
-	}
-}
-
-char				**ft_split(char const *s, char c)
-{
-	char			**tab;
-	char			*next_str;
-	unsigned int	next_str_len;
-	unsigned int	nb_strs;
-	unsigned int	i;
-
-	if (!s)
-		return (NULL);
-	nb_strs = ft_get_nb_strs(s, c);
-	if (!(tab = (char **)malloc(sizeof(char *) * (nb_strs + 1)))) //마지막 tab[i]에도 NULL보장
-		return (NULL);
-	i = 0;
-	next_str = (char *)s;
-	next_str_len = 0;
-	while (i < nb_strs)
-	{
-		ft_get_next_str(&next_str, &next_str_len, c);
-		if (!(tab[i] = (char *)malloc(sizeof(char) * (next_str_len + 1))))
-			return (ft_malloc_error(tab));
-		ft_strlcpy(tab[i], next_str, next_str_len + 1);
-		i++;
-	}
-	tab[i] = NULL;
-	return (tab);
+    find = c;
+    i = 0;
+    while (s[i] != '\0')
+    {
+        if (s[i] == find)
+            return ((char *)s + i);
+        i++;
+    }
+    if (s[i] == find)
+        return ((char *)s + i);
+    return (0);
 }
