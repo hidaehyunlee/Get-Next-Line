2895ab4a07cf3bb05cc978f3ee1ccebf6bcb2f11
leehi0210 <leehi0210@gmail.com>
2020-03-23 18:00:11 +0900

    modified split_line

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,21 +6,21 @@
 /*   By: daelee <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/11 17:57:29 by daelee            #+#    #+#             */
-/*   Updated: 2020/03/23 17:37:29 by daelee           ###   ########.fr       */
+/*   Updated: 2020/03/23 17:59:42 by daelee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-int                 split_line(char **backup, char **line, char *cut_addr)
+int                 split_line(char **backup, char **line, char *cut)
 {
 	char	        *temp;
 	size_t	        len;
 
-	if ((line = malloc(cut_addr - *backup + 1)) == 0)
+	if ((*line = (char *)malloc(ft_strlen(*backup) + 1)) == 0)
 		return (-1);
-	ft_strlcpy(*line, *backup, cut_addr - *backup + 1);
-	len = ft_strlen(cut_addr + 1);
+	ft_strlcpy(*line, *backup, ft_strlen(*backup) + 1);
+	len = ft_strlen(cut + 1);
 	if (len == 0)
 	{
 		free(*backup);
@@ -29,37 +29,13 @@ int                 split_line(char **backup, char **line, char *cut_addr)
 	}
 	if ((temp = malloc(len + 1)) == 0)
 		return (-1);
-	ft_strlcpy(temp, cut_addr + 1, len + 1);
+	ft_strlcpy(temp, cut + 1, len + 1);
 	free(*backup);
 	*backup = temp;
 	return (1);
 }
 
-// int                 get_next_line(int fd, char **line)
-// {
-//     char            *buf;
-//     static t_array  *backup;
-//     char            *cut_addr;
-//     int             eof;
-//     int             ret;
-
-// 	if (fd < 0 || line == 0 || BUFFER_SIZE <= 0)
-// 		return (-1);
-//     if (!(buf = (char *)malloc(sizeof(char)*BUFFER_SIZE + 1)))
-//         return (0);
-//     backup = ft_create_array();
-//     while (cut_addr = ft_strchr(backup->str, '\n') == 0)
-//     {
-//         eof = read(fd, buf, BUFFER_SIZE);
-//         ft_append_str(backup, buf);
-//     }
-//     ret = read_line(backup->str, *line, cut_addr);
-//     if (eof == 0)
-//         return (0);
-//     return (ret);
-// }
-
-int					append_backup(char *buf, char **backup, int read_size)
+int					append_backup(char **backup, char *buf, int read_size)
 {
 	char			*temp;
 	int 			temp_len;
@@ -87,7 +63,7 @@ int					append_backup(char *buf, char **backup, int read_size)
 int					get_next_line(int fd, char **line)
 {
 	static t_list 	list;
-	char			*cut;
+	char			*cut; //'\n' 개행문자가 나타나는 주소
 	int				read_size;
 
 	if ((fd < 0) || (line == 0) || (BUFFER_SIZE <= 0))
