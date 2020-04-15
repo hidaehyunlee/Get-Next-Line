66dc524074abdf1a7669831448c5dfaf07aa1a87
leehi0210 <leehi0210@gmail.com>
2020-04-15 22:15:15 +0900

    leak test

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: daelee <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/11 17:57:29 by daelee            #+#    #+#             */
-/*   Updated: 2020/04/15 22:07:52 by daelee           ###   ########.fr       */
+/*   Updated: 2020/04/15 22:14:54 by daelee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -61,16 +61,15 @@ int					return_zero(char **backup, char **line, int read_size)
 {
 	if (read_size < 0)
 		return (-1);
-	if (*backup == 0)
+	if (*backup != 0)
 	{
-		if ((*line = (char *)malloc(1)) == 0)
-			return (-1);
-		(*line)[0] = '\0';
+		*line = *backup;
+		*backup = 0;
 		return (0);
 	}
-	*line = *backup;
-	free(backup);
-	*backup = 0;
+	if ((*line = (char *)malloc(1)) == 0)
+		return (-1);
+	(*line)[0] = '\0';
 	return (0);
 }
 
